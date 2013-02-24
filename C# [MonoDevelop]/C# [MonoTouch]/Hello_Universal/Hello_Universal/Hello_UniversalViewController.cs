ffcab954603d5730a464e7059db47c2d77bc9339
khaledsaied <kaledsaied@gmail.com>
2013-02-24 16:40:48 +0100

    Adding HelloWorld Apps for iPad and iPhone using C# in MonoDevelop

---
+++ C# [MonoDevelop]/C# [MonoTouch]/Hello_Universal/Hello_Universal/Hello_UniversalViewController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Hello_Universal
+{
+	public partial class Hello_UniversalViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public Hello_UniversalViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "Hello_UniversalViewController_iPhone" : "Hello_UniversalViewController_iPad", null)
+		{
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			base.ViewDidLoad ();
+			this.btnClickMe.TouchUpInside += (sender, e) => {
+				this.lblOutput.Text = "Clicked @ " +
+					DateTime.Now.ToShortTimeString();
+			};
+		}
+		
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+			
+			// Clear any references to subviews of the main view in order to
+			// allow the Garbage Collector to collect them sooner.
+			//
+			// e.g. myOutlet.Dispose (); myOutlet = null;
+			
+			ReleaseDesignerOutlets ();
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
