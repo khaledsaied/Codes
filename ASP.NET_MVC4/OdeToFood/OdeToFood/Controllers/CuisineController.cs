6f6263a018ca2186e9d961407e0521db7a128cc8
khaledsaied <kaledsaied@gmail.com>
2013-04-01 19:17:31 +0200

    ASP.NET MVC 4 Tutorial

---
+++ ASP.NET_MVC4/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,40 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+       // [Authorize]
+        [Log]
+        public ActionResult Search(string name="arab")
+        {
+            throw new Exception("ERRRRRORR");
+
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+            //return RedirectPermanent("http://www.google.dk");
+            //return RedirectToAction("Index", "Home", new  { name = name});
+            //return RedirectToRoute("Default", new {controller = "Home", action = "About"});
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            //return Json(new {Message = message, Name = "Khaled"}, JsonRequestBehavior.AllowGet);
+        }
+
+        //[HttpGet]
+        //public ActionResult Search()
+        //{
+            
+        //    return Content("search");
+        //}
+
+    }
+}
